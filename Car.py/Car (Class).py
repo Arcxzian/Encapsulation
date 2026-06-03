d006948f48457418b97880f4076880b64a9fd1bc
Arcxzian <matthewcayno@gmail.com>
2026-06-03 21:44:10 +0800

    Add files via upload
    The class Car definition acts as a reusable blueprint for creating individual car object instances. Using the (__init__) uses the self keyword to dynamically bind specific initial values to each new object's memory space. and also Variables like self.__speed use double underscores to activate Python’s name-mangling, which locks out direct external modifications. A getter method (get_year_model) is introduced at the end of this block to allow controlled read-only access to this hidden data. and also the class provides clear public interfaces that hide data formatting details, such as rounding the fuel level directly inside the get_fuel method. This ensures that external code interacts with the object's behaviors rather than raw, unformatted data.

---
+++ Car.py/Car (Class).py
@@ -0,0 +1,40 @@
+class Car:
+    SPEED_LIMIT = 60
+    FUEL_CAPACITY = 50.0
+    FUEL_PER_ACCEL = 2.0
+    FUEL_PER_BREAK = 0.5
+
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+        self.__fuel = self.FUEL_CAPACITY
+        self.__warnings = []
+
+    def get_year_model(self):
+        return self.__year_model
+    
+    def get_make(self):
+        return self.__make
+    
+    def get_speed(self):
+        return self.__speed
+    
+    def get_fuel(self):
+        return round(self.__fuel, 1)
+    
+
+    def set_year_model(self, year_model):
+        self.__year_model = year_model
+
+    def set_make(self, make):
+        self.__make = make
+
+    def accelerate(self):
+        if self.__fuel <= 0:
+            print("OUT OF FUEL! cannot accelerate, Please refuel.")
+            return
+        self.__speed += 5
+        self.__fuel -= self.FUEL_PER_ACCEL
+        self.__fuel = max(self.__fuel, 0)
+        
