d6087c322cd71d5baa3ba8d10f6ca15c27d2d583
Arcxzian <matthewcayno@gmail.com>
2026-06-03 21:13:52 +0800

    add the slow mode
    test if slow mode is actually working from the class and to also check if the ASCII colors, and the bool argument is working

--- fan_class/Test_Fan.py
+++ fan_class/Test_Fan.py
@@ -3,9 +3,11 @@ from fan import Fan
 def Testfan():
     fan1 = Fan(speed=Fan.FAST, radius=10.0, color='yellow', on=True)
     fan2 = Fan(speed=Fan.MEDIUM, radius=5.0, color='blue', on=False)
+    fan3 = Fan(speed=Fan.SLOW, radius=2.0, color='violet', on=True)
 
     fan1.display_properties("Fan 1")
     fan2.display_properties("Fan 2")
+    fan3.display_properties("Fan 3")
 
 if __name__ == "__main__":
     Testfan()
